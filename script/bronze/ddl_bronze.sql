1f37148589f79182f12608e7263d30a7898654c7
Wera <wera.wawrzyczek@gmail.com>
2026-05-10 15:24:55 +0200

    Update ddl_bronze.sql

--- script/bronze/ddl_bronze.sql
+++ script/bronze/ddl_bronze.sql
@@ -20,7 +20,7 @@ CREATE TABLE bronze_crm_cust_info (
     cst_key VARCHAR(50),
     cst_firstname VARCHAR(50),
     cst_lastname VARCHAR(50),
-    cst_martial_status VARCHAR(50),
+    cst_marital_status VARCHAR(50),
     cst_gndr VARCHAR(50),
     cst_create_date DATE
 );
