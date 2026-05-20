b894de3d616a6c5b5511a81928569810a5439093
Wera <wera.wawrzyczek@gmail.com>
2026-05-20 19:03:42 +0200

    Update data_load_bronze.sql

--- script/bronze/data_load_bronze.sql
+++ script/bronze/data_load_bronze.sql
@@ -14,28 +14,28 @@ Script Purpose:
 
 -- Loading bronze_crm_cust_info
 TRUNCATE TABLE bronze_crm_cust_info; 
-LOAD DATA LOCAL INFILE '/Users/weronikawawrzyczek/Desktop/sql-data-warehouse-project/datasets/source_crm/cust_info.csv'
+LOAD DATA LOCAL INFILE '/path/to/your/repository/sql-data-analytics-project/datasets/flat-files/bronze_crm_cust_info.csv'
 INTO TABLE bronze_crm_cust_info
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
 -- Loading bronze_crm_prd_info
 TRUNCATE TABLE bronze_crm_prd_info; 
-LOAD DATA LOCAL INFILE '/Users/weronikawawrzyczek/Desktop/sql-data-warehouse-project/datasets/source_crm/prd_info.csv'
+LOAD DATA LOCAL INFILE '/path/to/your/repository/sql-data-analytics-project/datasets/flat-files/bronze_crm_prd_info.csv'
 INTO TABLE bronze_crm_prd_info
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
 -- Loading bronze_crm_cust_info
 TRUNCATE TABLE bronze_crm_cust_info; 
-LOAD DATA LOCAL INFILE '/Users/weronikawawrzyczek/Desktop/sql-data-warehouse-project/datasets/source_crm/sales_details.csv'
+LOAD DATA LOCAL INFILE '/path/to/your/repository/sql-data-analytics-project/datasets/flat-files/bronze_crm_cust_info.csv'
 INTO TABLE bronze_crm_sales_details
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
--- Loading bbronze_erp_cust_az12
+-- Loading bronze_erp_cust_az12
 TRUNCATE TABLE bronze_erp_cust_az12;
-LOAD DATA LOCAL INFILE '/Users/weronikawawrzyczek/Desktop/sql-data-warehouse-project/datasets/source_erp/CUST_AZ12.csv'
+LOAD DATA LOCAL INFILE '/path/to/your/repository/sql-data-analytics-project/datasets/flat-files/bronze_erp_cust_az12.csv'
 INTO TABLE bronze_erp_cust_az12
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
@@ -46,14 +46,14 @@ IGNORE 1 ROWS;
 
 -- Loading bronze_erp_loc_a101
 TRUNCATE TABLE bronze_erp_loc_a101;
-LOAD DATA LOCAL INFILE '/Users/weronikawawrzyczek/Desktop/sql-data-warehouse-project/datasets/source_erp/LOC_A101.csv'
+LOAD DATA LOCAL INFILE '/path/to/your/repository/sql-data-analytics-project/datasets/flat-files/bronze_erp_loc_a101.csv'
 INTO TABLE bronze_erp_loc_a101
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
 -- Loading bronze_erp_px_cat_g1v2
 TRUNCATE TABLE bronze_erp_px_cat_g1v2;
-LOAD DATA LOCAL INFILE '/Users/weronikawawrzyczek/Desktop/sql-data-warehouse-project/datasets/source_erp/PX_CAT_G1V2.csv'
+LOAD DATA LOCAL INFILE '/path/to/your/repository/sql-data-analytics-project/datasets/flat-files/bronze_erp_px_cat_g1v2.csv'
 INTO TABLE bronze_erp_px_cat_g1v2
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
